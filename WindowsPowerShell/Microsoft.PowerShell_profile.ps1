4545ba08088d3368ee5185210f8833ec4b9d87ed
Andreas Vifert <55092864+andreasgylche@users.noreply.github.com>
2023-06-27 20:16:16 +0200

    Update Microsoft.PowerShell_profile.ps1

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,8 @@
 Invoke-Expression (&starship init powershell)
 
-Import-Module Terminal-Icons
-
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -EditMode Windows
-
-Set-Alias ll ls
 Set-Alias g git
 
 function tconfig { code "C:\Users\Andreas\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" }
-function htdocs { Set-Location "C:\xampp\htdocs" }
-function lab { Set-Location "C:\Users\Andreas\Documents\webdev\_projects"}
-function repos { Set-Location "C:\Users\Andreas\repos"}
 function sc { Set-Location "C:\Users\Andreas\Documents\SkanCode"}
 function .. { Set-Location ".."}
 function ... { Set-Location "~"}
