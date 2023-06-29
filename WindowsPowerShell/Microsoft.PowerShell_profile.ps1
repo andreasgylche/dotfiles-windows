3e38a7eebdd788c326b0d810533679290ae075a0
Andreas Vifert <andreasgylche@gmail.com>
2023-06-29 03:43:28 +0200

    updated ps1 profile

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,8 @@ Invoke-Expression (&starship init powershell)
 
 Set-Alias g git
 
-function tconfig { code "C:\Users\Andreas\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" }
-function sc { Set-Location "C:\Users\Andreas\Documents\SkanCode"}
+function config { code $home\dev\dotfiles-windows }
+function dev { Set-Location $home\dev }
 function .. { Set-Location ".."}
 function ... { Set-Location "~"}
 function gs { git status }
