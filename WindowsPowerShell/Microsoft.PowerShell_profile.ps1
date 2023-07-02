d9dfe48ae566fb135eb1506c7035237c1ee856fb
Andreas Vifert <andreasgylche@gmail.com>
2023-07-02 12:47:10 +0200

    updated git aliases

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,16 @@
 Invoke-Expression (&starship init powershell)
 
-Set-Alias g git
-
-function config { code $home\dev\dotfiles-windows }
-function dev { Set-Location $home\dev }
-function .. { Set-Location ".."}
-function ... { Set-Location "~"}
-function gs { git status }
-function gcm { git commit -m $args }
-function ofd { explorer.exe . }
+# git aliases
+function gss { git status -s } # git status short version
+function gsl { git status --long } # git status long version
+function gaa { git add . } # git add all
+function gap { git add -p } # git add patch
+function gcm { git commit -m $args } # git commit + message
+function gpl { git pull } # git pull
+function gps { git push } # git push
+function gcl { git clone $args } # git clone + url
+function gco { git checkout $args } # git checkout + branch
+function glg { git lg } # git log graph (lg from .gitconfig)
 function gurl {
   $repoUrl = git config --get remote.origin.url
   if(!$repoUrl) {
@@ -22,6 +24,14 @@ function gurl {
 
   Start-Process $repoUrl 
 
-}
-function mdir { New-Item $args -Type Directory }
-function touch { New-Item $args -Type File }
+} # open git url
+
+# other aliases
+function config { code $home\dev\dotfiles-windows } # open dotfiles
+function dev { Set-Location $home\dev } # go to dev folder
+function .. { Set-Location ".."} # go up one folder
+function ... { Set-Location "~"} # go to home folder
+function ofd { explorer.exe . } # open current folder in explorer
+function mdir { New-Item $args -Type Directory } # make directory
+function touch { New-Item $args -Type File } # make file
+
